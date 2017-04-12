10fec0e7c9ebf6d5ef68b76a65cf6be58db2fa5d
wangzilaogong <997972547@qq.com>
2017-04-12 13:28:01 +0800

    index.js
    主要是重新写了下tree，用json传递值，然后关注每个树节点的点击打开tabs页面

--- js/index.js
+++ js/index.js
@@ -6,34 +6,7 @@ $(function(){
     $('#box').layout({
         fit : true//布局组件适应父容器
     });
-    //树形结构单击添加tabs
-    $('#tt1').tree({
-        onClick: function (node) {
-            if(!$('#tabs').tabs('exists',node.text)){
-                $('#tabs').tabs('add',{
-                    title:node.text,
-                    content:'aaa',
-                    href:'droppable.html',
-                    closable:true
-                });
-            }
-        }
-    });
-
-    $('#tt2').tree({
-        onClick: function (node) {
-            if(!$('#tabs').tabs('exists',node.text)){
-            $('#tabs').tabs('add',{
-                title:node.text,
-                content:'aaa',
-                href:'log.html',
-                closable:true
-            });}
-        }
-    });
-    $('#tt3').tree({
-        url:'tree_data3.json'
-    });
+    /*tabs工具栏*/
     $('#tabs').tabs({
         fit:true,
         tools:[
@@ -57,35 +30,65 @@ $(function(){
                     $('#tabs').tabs('update', {
                         tab: currentTab,
                         options: {
-                        href: url
-                    }
-                           });
+                            href: url
+                        }
+                    });
                     currentTab.panel('refresh');
                 }},
             {
                 iconCls:'icon-cross',
                 handler:function() {//关闭当前选择的tabs
-                                    //var tab = $('#tabs').tabs('getSelected');
-                                    //if (tab){
-                                    //    var index = $('#tabs').tabs('getTabIndex', tab);
-                                    //    $('#tabs').tabs('close', index);
+                    //var tab = $('#tabs').tabs('getSelected');
+                    //if (tab){
+                    //    var index = $('#tabs').tabs('getTabIndex', tab);
+                    //    $('#tabs').tabs('close', index);
                     $("#tabs li").each(function(index, obj) {
-                         //获取所有可关闭的选项卡
-                     var tab = $(".tabs-closable", this).text();
-                         $("#tabs").tabs('close', tab);
-                });
-                    }
+                        //获取所有可关闭的选项卡
+                        var tab = $(".tabs-closable", this).text();
+                        $("#tabs").tabs('close', tab);
+                    });
                 }
+            }
 
         ]});
-
-    $('#mainPanle').panel({
-        tools:[{
-            iconCls:'icon-add',
-            handler:function(){alert('new')}
-        },{
-            iconCls:'icon-save',
-            handler:function(){alert('save')}
-        }]
+    /*树形结构单击添加tabs*/
+    $('#ttPeople,#ttMessage,#ttLog,#ttSetting,#ttPeople,#ttBase,#ttSystem').tree({
+        onClick: function (node) {
+            if(!$('#tabs').tabs('exists',node.text)){
+                $('#tabs').tabs('add',{
+                    title:node.text,
+                    href:node.attributes.url,
+                    closable:true
+                });
+            }
+        }
     });
+
+    /*系统管理*/
+    $('#ttSystem').tree({
+        url:'json/ttSystem.json'
+    });
+    /*基础信息*/
+    $('#ttBase').tree({
+        url:'json/ttBase.json'
+    });
+    /*人员管理*/
+    $('#ttPeople').tree({
+        url:'json/ttPeople.json'
+    });
+    /*报警设置*/
+    $('#ttSetting').tree({
+        url:'json/ttSetting.json'
+    });
+    /*日志系统*/
+    $('#ttLog').tree({
+        url:'json/ttLog.json'
+    });
+    /* 发送消息*/
+    $('#ttMessage').tree({
+        url:'json/ttMessage.json'
+    });
+
+
+
 });
